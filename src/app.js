e8ed7130f8ff58d9b5d68392a12e5045aaa97654
Piyush Singh <piyushkumarsingh.pks31@gmail.com>
2026-07-01 00:53:14 +0530

    Playing With the Routes

--- src/app.js
+++ src/app.js
@@ -2,28 +2,25 @@ const express = require('express');
 
 const app = express();
 
-app.use("/hello", (req,res) => {
-    res.send("Hello Hello Hello");
-});
-
 // This will only handle GET call to /user
-app.get("/user" , (req,res) => {
+app.get("/user/:userId/:Name/:Pass" , (req,res) => {
+    console.log(req.params);
     res.send({firstName: "Piyush" , lastName: "Singh"});
 });
 
 app.post("/user" , (req,res) => {
     console.log("Save the data to the database");
     res.send("Data successfully saved to the database!");
+});
+
+app.delete("/user" , (req,res) => {
+    res.send("Deleted Successfully");
 })
 
 // This will match all the HTTP methods API calls to /test
 app.use("/test", (req,res) => {
     res.send("Hello from the server!");
 });
-
-app.use("/" , (req,res) => {
-    res.send("Hello from the dashboard");
-})
 
 app.listen(7777 , () => {
     console.log("Server is listening successfully on port 7777...");
