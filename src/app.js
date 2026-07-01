6d11d40ae149c38aae1e3f43537905b40f00481c
Piyush Singh <piyushkumarsingh.pks31@gmail.com>
2026-07-01 23:17:28 +0530

    Multiple-Route-Handler

--- src/app.js
+++ src/app.js
@@ -2,25 +2,16 @@ const express = require('express');
 
 const app = express();
 
-// This will only handle GET call to /user
-app.get("/user/:userId/:Name/:Pass" , (req,res) => {
-    console.log(req.params);
-    res.send({firstName: "Piyush" , lastName: "Singh"});
-});
-
-app.post("/user" , (req,res) => {
-    console.log("Save the data to the database");
-    res.send("Data successfully saved to the database!");
-});
-
-app.delete("/user" , (req,res) => {
-    res.send("Deleted Successfully");
-})
-
-// This will match all the HTTP methods API calls to /test
-app.use("/test", (req,res) => {
-    res.send("Hello from the server!");
-});
+app.use("/user" , (req,res,next) => {
+    console.log("Headling the route user1!");
+    res.send("Response!!");
+    next(); 
+},
+(req,res) => {
+    console.log("Handling the route user2!");
+    res.send("2nd Response");
+}
+);
 
 app.listen(7777 , () => {
     console.log("Server is listening successfully on port 7777...");
