af94fc503896782fd0ca826656af73cfe8d2dd62
Piyush Singh <piyushkumarsingh.pks31@gmail.com>
2026-07-05 16:02:01 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -21,6 +21,48 @@ app.post("/signup" , async (req,res) => {
     }
  });
 
+
+app.get("/user" , async (req,res) => {
+    const UserEmail = req.body.emailId;
+
+    try {
+
+        const user = await User.findOne({emailId: UserEmail});
+        res.send(user);
+        if(!user) {
+            res.status(404).send("User Not Found!");
+        }
+        else {
+            res.send(user);
+        }
+        // const users = await User.find({emailId: UserEmail});
+        // if(users.length == 0) {
+        //     res.status(404).send("User Not Found!");
+        // }
+        // else {
+        //     res.send(users);
+        // }
+     }catch(err) { 
+        res.status(400).send("Something went wrong!!");
+     }
+
+
+})
+
+// Feed API -> GET / feed - get all the users from the database
+app.get("/feed" , async (req,res) => {
+
+    try {
+        const users = await  User.find({});
+        res.send(users);
+
+    }catch(err) { 
+        res.status(400).send("Something went wrong!!");
+    }
+
+
+})
+
 connectDB().then(() => {
     console.log("Database connected successfully!");
     app.listen(7777 , () => {
